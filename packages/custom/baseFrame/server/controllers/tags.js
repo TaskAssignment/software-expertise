c0b3edcaded056adbae3bb6e4b33bb7cd340a61f
Parley Martins <parleypachecomartins@gmail.com>
2016-05-02 13:48:26 -0600

    Split routes and controllers

--- packages/custom/baseFrame/server/controllers/tags.js
+++ packages/custom/baseFrame/server/controllers/tags.js
@@ -6,10 +6,114 @@ var Tag = mongoose.model('Tag');
 var config = require('meanio').loadConfig();
 var _ = require('lodash');
 
-module.exports = function (Tags){
-    return {
+var fs = require('fs');
+var cf = require('crossfilter');
+var d3 = require('d3');
 
-        /** Creates a new Tag
-        create: function (){}
-    };
+module.exports = function (Tags){
+    console.log('load data files 0/3');
+    var coOccurrencesData;
+    var fileName = 'coOccurrences.tsv';
+    var coOccurrences;
+    var tag1Filter;
+    var tag2Filter;
+    var coOccurrenceFilter;
+
+    var commonUserDataCF;
+    var commonUserFilter;
+    var TagCountServices = {};
+    var graphConfig;
+
+    fs.readFile(fileName, 'utf8', function (err, result){
+      if(err)  console.error(err);
+      else {
+        result = d3.tsv.parse(result);
+        console.log('load data files 1/3');
+        coOccurrencesData = result;
+        coOccurrences = cf(result);
+        tag1Filter = coOccurrences.dimension(function(d) { return d; });
+        coOccurrenceFilter = coOccurrences.dimension(function(d) { return d.CoOccurrence; });
+        coOccurrenceFilter.filter( function(d){ return d >= 10; } );
+        fs.readFile('tags.tsv', 'utf8', function (err, result){
+          if(err) console.error(err);
+          else {
+            var convertResults = d3.tsv.parse(result);
+
+            console.log('load data files 2/3');
+            for(var i = 0; i < convertResults.length; i++){
+              var tag = convertResults[i];
+              TagCountServices[tag.TagName] = tag.Count
+            }
+            fs.readFile('commonUsers.tsv', 'utf8', function(err, result){
+              if(err) console.error(err);
+              else {
+                var commonUserResult = d3.tsv.parse(result);
+                console.log('load data files 3/3');
+
+                //Save this to Database!!!
+                commonUserDataCF = cf(commonUserResult);
+                commonUserFilter = commonUserDataCF.dimension(function(d) { return d.login; });
+              }
+            });
+          }
+        });
+      }
+    });
+
+    return {
+        soTags: function(req,res,next){
+            res.send(JSON.stringify(TagCountServices));
+        },
+
+        soIDFromUser: function(req, res, next){
+            if(req.body.getAll === 'true'){
+                commonUsers.filter(null);
+            } else {
+                commonUserFilter.filter(req.body.gitName);
+            }
+            res.send(JSON.stringify(commonUserFilter.top(Infinity)));
+        },
+
+        coOccurrence: function (req, res, next) {
+            var stringRes = '';
+            if (req.body['getEverything'] === 'true'){
+
+                 tag1Filter.filter(null);
+
+                 var f1Data =tag1Filter.top(Infinity);
+
+              res.send(JSON.stringify(f1Data));
+              tag1Filter.filter();
+
+            } else {
+              var tagsToFilter = [];
+              // console.log(req.body);
+              for(var key in req.body) {
+                  if(req.body.hasOwnProperty(key))
+                  {
+                      tagsToFilter = req.body[key];
+                  }
+              }
+              //These tags come from 'displayIssueTags' on repository.js
+              console.log(tagsToFilter);
+
+              tag1Filter.filter(function(d,i){
+                  if(tagsToFilter.indexOf(d.Tag1) !== -1 &&
+                      tagsToFilter.indexOf(d.Tag2) !== -1){
+                      return true;
+                  } else {
+                      return false;
+                  }
+              });
+
+              var f1Data = tag1Filter.top(Infinity);
+              console.log(f1Data);
+
+
+              res.send(JSON.stringify(f1Data));
+              tag1Filter.filter();
+            }
+
+        }
+    }
 }
--- packages/custom/baseFrame/server/routes/tags.js
+++ packages/custom/baseFrame/server/routes/tags.js
@@ -1,126 +1,14 @@
-var fs = require('fs');
-var cf = require('crossfilter');
+'use strict';
 var d3 = require('d3');
 var http = require('http');
 
 var https = require('https');
-(function () {
-    'use strict';
-    Array.prototype.getUnique = function(){
-        var u = {}, a = [];
-        for(var i = 0, l = this.length; i < l; ++i){
-          if(u.hasOwnProperty(this[i])) {
-             continue;
-          }
-          a.push(this[i]);
-          u[this[i]] = 1;
-        }
-        return a;
-    }
-  /* jshint -W098 */
-  // The Package is past automatically as first parameter
-  module.exports = function (BaseFrame, app, auth, database) {
-    console.log('load data files 0/3');
-    var coOccurrencesData;
-    var fileName = 'coOccurrences.tsv';
-    var coOccurrences;
-    var tag1Filter;
-    var tag2Filter;
-    var coOccurrenceFilter;
-
-    var commonUserDataCF;
-    var commonUserFilter;
-    var TagCountServices = {};
-    var graphConfig;
-
-    fs.readFile(fileName, 'utf8', function (err, result){
-        if(err)  console.error(err);
-        else {
-            result = d3.tsv.parse(result);
-            console.log('load data files 1/3');
-            coOccurrencesData = result;
-            coOccurrences = cf(result);
-            tag1Filter = coOccurrences.dimension(function(d) { return d; });
-            coOccurrenceFilter = coOccurrences.dimension(function(d) { return d.CoOccurrence; });
-            coOccurrenceFilter.filter( function(d){ return d >= 10; } );
-            fs.readFile('tags.tsv', 'utf8', function (err, result){
-                if(err) console.error(err);
-                else {
-                    var convertResults = d3.tsv.parse(result);
-
-                    console.log('load data files 2/3');
-                    for(var i = 0; i < convertResults.length; i++){
-                        var tag = convertResults[i];
-                        TagCountServices[tag.TagName] = tag.Count
-                    }
-                    fs.readFile('commonUsers.tsv', 'utf8', function(err, result){
-                        if(err) console.error(err);
-                        else {
-                            var commonUserResult = d3.tsv.parse(result);
-                            console.log('load data files 3/3');
-
-                            //Save this to Database!!!
-                            commonUserDataCF = cf(commonUserResult);
-                            commonUserFilter = commonUserDataCF.dimension(function(d) { return d.login; });
-                        }
-                    });
-                }
-            });
-        }
-    });
-
-    app.post('/api/baseFrame/soTags',function(req,res,next){
-        res.send(JSON.stringify(TagCountServices));
-    });
-
-    app.post('/api/baseFrame/soIDFromUser', function(req, res, next){
-        if(req.body.getAll === 'true'){
-            commonUsers.filter(null);
-        } else {
-            commonUserFilter.filter(req.body.gitName);
-        }
-        res.send(JSON.stringify(commonUserFilter.top(Infinity)));
-    });
-    app.post('/api/baseFrame/coOccurrence', function (req, res, next) {
-        var stringRes = '';
-        if (req.body['getEverything'] === 'true'){
-
-             tag1Filter.filter(null);
-
-             var f1Data =tag1Filter.top(Infinity);
-
-          res.send(JSON.stringify(f1Data));
-          tag1Filter.filter();
-
-        } else {
-          var tagsToFilter = [];
-          // console.log(req.body);
-          for(var key in req.body) {
-              if(req.body.hasOwnProperty(key))
-              {
-                  tagsToFilter = req.body[key];
-              }
-          }
-          //These tags come from 'displayIssueTags' on repository.js
-          console.log(tagsToFilter);
-
-          tag1Filter.filter(function(d,i){
-              if(tagsToFilter.indexOf(d.Tag1) !== -1 &&
-                  tagsToFilter.indexOf(d.Tag2) !== -1){
-                  return true;
-              } else {
-                  return false;
-              }
-          });
-
-          var f1Data = tag1Filter.top(Infinity);
-          console.log(f1Data);
 
 
-          res.send(JSON.stringify(f1Data));
-          tag1Filter.filter();
-        }
+module.exports = function (BaseFrame, app, auth, database) {
+    var tags = require('../controllers/tags')(BaseFrame);
 
-    });
-  };
-})();
+    app.post('/api/baseFrame/soTags', tags.soTags);
+    app.post('/api/baseFrame/soIDFromUser', tags.soIDFromUser);
+    app.post('/api/baseFrame/coOccurrence', tags.coOccurrence);
+};
