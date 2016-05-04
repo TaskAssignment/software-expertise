a659fb60003bdcdc4415188fa2b5566306777d4d
Parley Martins <parleypachecomartins@gmail.com>
2016-05-04 15:04:29 -0600

    Remove unused file and add print to know how the home route behaves

--- packages/core/system/server/controllers/index.js
+++ packages/core/system/server/controllers/index.js
@@ -3,11 +3,12 @@
 var mean = require('meanio');
 module.exports = function(System){
   return {
-    render:function(req,res){
-      res.render('index',{ locals: { config: System.config.clean }});
-    },
-    aggregatedList:function(req,res) {
-      res.send(res.locals.aggregatedassets);
-    }
+      render:function(req,res){
+          console.log(req.query);
+          res.render('index',{ locals: { config: System.config.clean }});
+      },
+      aggregatedList:function(req,res) {
+          res.send(res.locals.aggregatedassets);
+      }
   };
 };
