06f4410688b71cd9870edc45210f1c0b0620e7ce
Parley Martins <parleypachecomartins@gmail.com>
2016-05-05 15:51:22 -0600

    Add a new route to handle the requests properly

--- packages/custom/baseFrame/public/routes/baseFrame.js
+++ packages/custom/baseFrame/public/routes/baseFrame.js
@@ -1,4 +1,10 @@
 'use strict';
 
-//I still don't know how to connect this with the server routes. 
-angular.module('mean.baseFrame');
+//I still don't know how to connect this with the server routes.
+angular.module('mean.baseFrame').config(['$stateProvider',
+function ($stateProvider){
+    $stateProvider.state('Expertise', {
+        url: '/expertise',
+        templateUrl: 'baseFrame/views/index.html'
+    });
+}]);
--- packages/custom/baseFrame/server/routes/baseFrame.js
+++ packages/custom/baseFrame/server/routes/baseFrame.js
@@ -1,24 +1,30 @@
 'use strict';
 
 module.exports = function (BaseFrame, app, auth, database) {
+    app.route('/expertise').get(function(res, req){
+        console.log(req.query);
+        console.log(req);
+        res.json("OK");
+    });
     var controllers = '../controllers/';
     var tags = require(controllers + 'tags')(BaseFrame);
 
     app.route('/api/baseFrame/populateSoTags')
-      .post(tags.populateSoTags);
+        .post(tags.populateSoTags);
     app.route('/api/baseFrame/populateCoOccurrences')
-      .post(tags.populateCommonOccurrences);
+        .post(tags.populateCommonOccurrences);
     app.route('/api/baseFrame/populateSoUsers')
-      .post(tags.populateSoUsers);
+        .post(tags.populateSoUsers);
     app.route('/api/baseFrame/getIssueTags')
-      .post(tags.getIssueTags);
+        .post(tags.getIssueTags);
     app.route('/api/baseFrame/soIDFromUser')
-      .post(tags.soIDFromUser);
+        .post(tags.soIDFromUser);
     app.route('/api/baseFrame/coOccurrence')
-      .post(tags.coOccurrence);
+        .post(tags.coOccurrence);
 
     var stopwords = require(controllers + 'stopWords')(BaseFrame);
 
     app.route('/api/baseFrame/populateStopWords')
-      .post(stopwords.populateStopWords);
+        .post(stopwords.populateStopWords);
+
 };
