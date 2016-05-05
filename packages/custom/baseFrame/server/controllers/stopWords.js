e61c074dcc207da4010450d0b39cceab123e3da3
Parley Martins <parleypachecomartins@gmail.com>
2016-05-05 10:24:15 -0600

    Change stopwords file structure and finish populate them

--- packages/custom/baseFrame/server/controllers/stopWords.js
+++ packages/custom/baseFrame/server/controllers/stopWords.js
@@ -10,33 +10,31 @@ module.exports = function (BaseFrame) {
         populateStopWords: function(req, res){
             var folder = 'files/stopwords/';
             fs.readdir(folder, function (err, files){
+                var stopwords = [];
+
                 for(var i in files){
                     var file = files[i];
-                    fs.readFile(folder + file, 'utf8', function(err, data){
-                        if(err) {
-                            console.log(err);
-                            return res.sendStatus(500);
-                        }
-                        //Get the words and remove last empty line
-                        var words = data.split('\n').slice(0, -1);
+                    // reading syncronous to make only one database request
+                    var data = fs.readFileSync(folder + file, 'utf8');
+                    var words = data.split(',');
 
-                        var stopwords = [];
-                        for(var j in words){
-                            var stopword = {}
-                            stopword['word'] = words[j];
+                    for(var j in words){
+                        var stopword = {}
+                        stopword['word'] = words[j];
 
-                            stopwords.push(stopword);
-                        }
-
-                        StopWord.collection.insert(stopwords, function(err){
-                            if(err){
-                                console.log(err.message);
-                            }else{
-                                console.log('Stop Words saved successfully!');
-                            }
-                        });
-                    });
+                        stopwords.push(stopword);
+                    }
                 }
+                console.log(stopwords);
+                console.log(stopwords.length);
+
+                StopWord.collection.insert(stopwords, function(err){
+                    if(err){
+                        console.log(err.message);
+                    }else{
+                        console.log('Stop Words saved successfully!');
+                    }
+                });
                 res.sendStatus(200);
             });
         },
