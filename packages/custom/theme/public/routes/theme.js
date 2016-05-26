267ec5ef75611aa0e368961ab903f5cc796d5a7a
Parley Martins <parleypachecomartins@gmail.com>
2016-05-26 13:33:35 -0600

    Remove package theme

--- packages/custom/theme/public/routes/theme.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-// 
-// angular.module('mean.theme').config(['$meanStateProvider','$viewPathProvider',
-// function($meanStateProvider,$viewPathProvider){
-//     $viewPathProvider.override('system/views/index.html', 'theme/views/index.html')
-// }]);
