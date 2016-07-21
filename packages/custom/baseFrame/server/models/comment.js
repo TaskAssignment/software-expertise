c76ae5e5ef8f8e3effc55462f5ce94e9cd250110
Parley Martins <parleypachecomartins@gmail.com>
2016-07-21 11:01:31 -0600

    Update Comment documentation

--- packages/custom/baseFrame/server/models/comment.js
+++ packages/custom/baseFrame/server/models/comment.js
@@ -13,19 +13,76 @@ var Schema = mongoose.Schema;
 * @requires mongoose
 **/
 var CommentSchema = new Schema({
+    /** The id on GitHub
+    *
+    * @inner
+    * @memberof Comment
+    * @type  {Number}
+    **/
     _id: Number,
+
+    /** The comment itself
+    *
+    * @inner
+    * @memberof Comment
+    * @type  {String}
+    **/
     body: String,
+
+    /** The username of the user who made this comment
+    *
+    * @inner
+    * @memberof Comment
+    * @type  {String}
+    **/
     user: String,
+
+    /** When this comment was created
+    *
+    * @inner
+    * @memberof Comment
+    * @type  {Date}
+    **/
     createdAt: Date,
+
+    /** If this comment was made on an issue, this is the number of that issue.
+    * If it is on a commit this will be null.
+    *
+    * @inner
+    * @memberof Comment
+    * @type  {Number}
+    **/
     issueNumber: Number,
+
+    /** If this comment was made on a commit, this is the commit sha.
+    * If it is on an issue this will be null.
+    *
+    * @inner
+    * @memberof Comment
+    * @type  {String}
+    **/
     commitSha: {
         type: String,
         ref: 'Commit'
     },
+
+    /** The id of the project where this issue occurs.
+    *
+    * @inner
+    * @memberof Comment
+    * @type  {Number}
+    **/
     projectId: {
         type: Number,
         ref: 'Issue'
     },
+
+    /** The type of comment. It can be 'issue' or 'commit'
+    *
+    * @inner
+    * @memberof Comment
+    * @type  {String}
+    **/
     type: String //Issue comment or commit comment
 });
 
