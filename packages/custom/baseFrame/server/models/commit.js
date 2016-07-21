568aded7bb4d9f38c8d36e18d83851fe9a9945da
Parley Martins <parleypachecomartins@gmail.com>
2016-07-21 11:08:27 -0600

    Update Commit documentation

--- packages/custom/baseFrame/server/models/commit.js
+++ packages/custom/baseFrame/server/models/commit.js
@@ -10,14 +10,57 @@ belongs to. Message is required.
 * @requires mongoose
 **/
 var CommitSchema = new Schema({
+    /** This is the sha from the commit on GitHub. Required
+    *
+    * @inner
+    * @memberof Commit
+    * @type {String}
+    **/
     _id: String,
+
+    /** The message of the commit. Required.
+    *
+    * @inner
+    * @memberof Commit
+    * @type {String}
+    **/
     message: String,
+
+    /** The username of the user that authored this commit.
+    * If that option is blank, for some reason, the commiter will be saved here.
+    *
+    * @inner
+    * @memberof Commit
+    * @type {String}
+    **/
     user: String,
+
+    /** The project where this commit comes from.
+    *
+    * @inner
+    * @memberof Commit
+    * @type {Number}
+    * @see Project
+    **/
     projectId: {
         type: Number,
         ref: 'Project',
     },
+
+    /** The date that this commit was created.
+    *
+    * @inner
+    * @memberof Commit
+    * @type {Date}
+    **/
     createdAt: Date,
+
+    /** The url to see this on GitHub.
+    *
+    * @inner
+    * @memberof Commit
+    * @type {String}
+    **/
     url: String
 });
 
