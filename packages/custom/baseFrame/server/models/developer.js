9c11c5dc9bac01e5f9aa51fba12d25f0f50f4c6d
Parley Martins <parleypachecomartins@gmail.com>
2016-07-21 11:47:16 -0600

    Update Developer documentation

--- packages/custom/baseFrame/server/models/developer.js
+++ packages/custom/baseFrame/server/models/developer.js
@@ -5,18 +5,35 @@ var Schema = mongoose.Schema;
 
 /** This is the generic Developer profile. It gathers all the other profiles based
 * on the user's email. One developer can have several profiles on different platforms,
-* like GitHub and StackOverflow. Any other profiles must be added to this and referenced
-* on their models.
+* like GitHub and StackOverflow. Any other profiles must be referenced on this schema.
 *
 * @class Developer
 * @requires mongoose
 **/
 var DeveloperSchema = new Schema({
+    /** The user email
+    *
+    * @inner
+    * @memberof Developer
+    * @type {String}
+    **/
     email: {
         type: String,
         unique: true,
         required: true,
     },
+
+    /** This will have the references to all the profiles found with this email.
+    * Right now, we have references to GitHub, StackOverflow and Bugzilla.
+    * If a new profile is added, it must be referenced here.
+    *
+    * @inner
+    * @memberof Developer
+    * @type  {Object}
+    * @see GitHubProfile
+    * @see StackOverflowProfile
+    * @see BugzillaProfile
+    **/
     profiles: {
         gh: [{
             type: String,
