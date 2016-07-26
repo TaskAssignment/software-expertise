810974613aa76b6c48ddf22dbcc768b5342873d9
Parley Martins <parleypachecomartins@gmail.com>
2016-07-26 11:17:14 -0600

    Add import/export pages

--- packages/custom/baseFrame/public/routes/baseFrame.js
+++ packages/custom/baseFrame/public/routes/baseFrame.js
@@ -1,13 +1,15 @@
 'use strict';
 
-//I still don't know how to connect this with the server routes.
 angular.module('mean.baseFrame').config(['$stateProvider',
 function ($stateProvider){
     $stateProvider.state('Expertise', {
         url: '/expertise',
         templateUrl: 'baseFrame/views/index.html'
-    }).state('Admin Tasks', {
-        url: '/admin',
-        templateUrl: 'baseFrame/views/admin.html'
+    }).state('Import Data', {
+        url: '/import',
+        templateUrl: 'baseFrame/views/import.html'
+    }).state('Export Data', {
+        url: '/export',
+        templateUrl: 'baseFrame/views/export.html'
     });
 }]);
