bd675cfd4de6e425aa3a53efba2c44733555f76c
Parley Martins <parleypachecomartins@gmail.com>
2016-08-16 15:47:30 -0600

    Install python packages as system packages where possible

--- cookbooks/dependencies/recipes/default.rb
+++ cookbooks/dependencies/recipes/default.rb
@@ -25,18 +25,20 @@ package "python-dev"
 package "libxml2-dev"
 package "libxslt1-dev"
 package "zlib1g-dev"
+package "python3-lxml"
+package "python3-requests"
 
 
 execute 'Install global npm packages' do
   command 'npm install -g gulp forever'
 end
 
-execute 'Install npm packages' do
-  command 'npm install'
-  cwd '/vagrant'
+execute 'Install python dependencies' do
+    command 'pip3 install -r packages/custom/expertise/server/bugzilla-python/requirements.txt'
+    cwd '/vagrant'
 end
 
-execute 'Install python dependencies' do
-  command 'pip3 install -r packages/custom/expertise/server/bugzilla-python/requirements.txt'
+execute 'Install npm packages' do
+  command 'npm install'
   cwd '/vagrant'
 end
